864c397d3ba26f9c5fd293dff3444dd368ed181d
Juan Martinez <jkmilomss@gmail.com>
2022-11-20 00:59:14 -0500

    Tiempo

--- app/src/main/java/com/example/game/juego.kt
+++ app/src/main/java/com/example/game/juego.kt
@@ -2,12 +2,14 @@ package com.example.game
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 
 class juego : AppCompatActivity() {
-    var puntos:Int
+    var puntos:Int=0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_juego)
@@ -15,8 +17,23 @@ class juego : AppCompatActivity() {
         var intencion=intent
         val re:String? = intencion.getStringExtra("entrada")
         titulo.text=re
+        puntos=0
+
+        var tie:TextView=findViewById(R.id.tiempo) as TextView
+        object :CountDownTimer(10000,1000){
+            override fun onFinish() {
+                tie.text="Se acabo el tiempo"
+            }
+
+            override fun onTick(p0: Long) {
+                tie.text="Tiempo: " + p0/1000
+            }
+        }.start()
     }
     fun puntos(view: View){
+       puntos ++
+        var pun:TextView=findViewById(R.id.puntajeu) as TextView
+        pun.text="Puntos: "+puntos.toString()
 
     }
 }
