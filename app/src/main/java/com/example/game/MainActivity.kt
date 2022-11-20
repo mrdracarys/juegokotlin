e18e5b639895f9963e6ce7c1e4528a4bb8604413
Juan Martinez <jkmilomss@gmail.com>
2022-11-19 23:03:39 -0500

    Empiezo de codigo

--- app/src/main/java/com/example/game/MainActivity.kt
+++ app/src/main/java/com/example/game/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.game
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun cambiarventana(view: View){
+        val inte=Intent(applicationContext,juego::class.java)
+        val nombre:EditText=findViewById(R.id.nombre) as EditText
+        val edd:EditText=findViewById(R.id.edad) as EditText
+        inte.putExtra("entrada",nombre.text.toString())
+        inte.putExtra("edad",edd.text.toString())
+        startActivity(inte)
     }
 }
--- app/src/main/java/com/example/game/juego.kt
+++ app/src/main/java/com/example/game/juego.kt
@@ -2,10 +2,18 @@ package com.example.game
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class juego : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_juego)
+        var titulo:TextView=findViewById(R.id.nombreu) as TextView
+        var intencion=intent
+    }
+    fun puntos(view: View){
+
     }
 }
